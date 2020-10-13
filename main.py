839c69a2390215c75872e1886314ff7cca77782f
James <jwitt@wpi.edu>
2020-10-13 18:19:43 -0400

    images loaded in

--- main.py
+++ main.py
@@ -4,8 +4,16 @@ import numpy as np
 
 # Model Template
 
-model = Sequential() # declare model
-model.add(Dense(10, input_shape=(28*28, ), kernel_initializer='he_normal')) # first layer
+images = np.load("images.npy")
+
+vecArr = []
+for i in images:
+    newArr = i.reshape(-1)
+    vecArr.append(newArr)
+print(vecArr[0].size)
+
+model = Sequential()  # declare modelnd
+model.add(Dense(10, input_shape=(28 * 28,), kernel_initializer='he_normal'))  # first layer
 model.add(Activation('relu'))
 #
 #
@@ -13,9 +21,8 @@ model.add(Activation('relu'))
 # Fill in Model Here
 #
 #
-model.add(Dense(10, kernel_initializer='he_normal')) # last layer
+model.add(Dense(10, kernel_initializer='he_normal'))  # last layer
 model.add(Activation('softmax'))
-
 
 # Compile Model
 model.compile(optimizer='sgd',
@@ -23,13 +30,13 @@ model.compile(optimizer='sgd',
               metrics=['accuracy'])
 
 # Train Model
-history = model.fit(x_train, y_train,
-                    validation_data = (x_val, y_val),
-                    epochs=10,
-                    batch_size=512)
+# history = model.fit(x_train, y_train,
+#                     validation_data = (x_val, y_val),
+#                     epochs=10,
+#                     batch_size=512)
 
 
 # Report Results
 
-print(history.history)
-model.predict()
+# print(history.history)
+# model.predict()
